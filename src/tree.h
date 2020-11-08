4aee6f70bc19d96a086b6bb23a216b73d5fd4aca
Oscar Roisin <oscar.roisin@etu.utc.fr>
2020-11-08 15:52:23 +0000

    add expansion function, constructor and destructor for node

--- src/tree.h
+++ src/tree.h
@@ -12,7 +12,31 @@ struct Node
   int N;
   struct Node *parent;
   std::vector<struct Node *> children;
-  int nextToExpand = 0;
+  int nextToExpand;
+  Node(int current, struct Node *p)
+  {
+    currentLocation = current;
+    Q = 0;
+    N = 0;
+    nextToExpand = 0;
+    parent = p;
+  }
+  ~Node()
+  {
+    for (auto it = begin(children); it != end(children); ++it)
+    {
+      delete it;
+    }
+    delete this;
+  }
+  void expand(std::vector<int> &c)
+  {
+    for (auto it = begin(c); it != end(c); ++it)
+    {
+      Node *child = new Node(it, this);
+      children.push_back(child)
+    }
+  }
   struct Node *getNext()
   {
     return children[nextToExpand++];
