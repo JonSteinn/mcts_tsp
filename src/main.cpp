7bfadead62a70b8c3d0d6bf44ac0b2defd4e53fc
jonsteinn <jonsteinn@gmail.com>
2020-11-08 17:08:26 +0000

    edit config

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "tsp.h"
+
 #include <iostream>
 
 int main() {
-    TSP tsp1("data/tour001.txt");
-    return 0;
+  TSP tsp1("data/tour001.txt");
+  return 0;
 }
--- src/tsp.cpp
+++ src/tsp.cpp
@@ -2,63 +2,63 @@
 
 
 TSP::TSP(std::string file_name) {
-    std::string line;
-    std::ifstream tsp_file(file_name);
-    int data_points = 0;
-    std::vector<std::tuple<float, float>> data;
-    
-    if (tsp_file.is_open())
-    {
-        std::getline(tsp_file, line);
-        this->optimal_length = std::stof(line);
-        std::getline(tsp_file, line);
-        data_points = std::stoi(line);
-        for (int i = 0; i < data_points; i++) {
-            std::getline(tsp_file, line);
-            int delim_pos = line.find(" ");
-            float x = std::stof(line.substr(0, delim_pos));
-            float y = std::stof(line.substr(delim_pos + 1));
-            data.push_back(std::make_tuple(x, y));
-        }
-        tsp_file.close();
-    }
+  std::string line;
+  std::ifstream tsp_file(file_name);
+  int data_points = 0;
+  std::vector<std::tuple<float, float>> data;
 
-
+  if (tsp_file.is_open())
+  {
+    std::getline(tsp_file, line);
+    this->optimal_length = std::stof(line);
+    std::getline(tsp_file, line);
+    data_points = std::stoi(line);
     for (int i = 0; i < data_points; i++) {
-        this->matrix.push_back(std::vector<float>(data_points, 0.0f));
+      std::getline(tsp_file, line);
+      int delim_pos = line.find(" ");
+      float x = std::stof(line.substr(0, delim_pos));
+      float y = std::stof(line.substr(delim_pos + 1));
+      data.push_back(std::make_tuple(x, y));
     }
+    tsp_file.close();
+  }
 
-    for (int i = 0; i < data_points; i++) { 
-        for (int j = i + 1; j < data_points; j++) {
-            float dx = std::get<0>(data[i]) - std::get<0>(data[j]);
-            float dy = std::get<1>(data[i]) - std::get<1>(data[j]);
-            this->matrix[i][j] = sqrtf(dx * dx + dy * dy);
-            this->matrix[j][i] = this->matrix[i][j];
-        }
+
+  for (int i = 0; i < data_points; i++) {
+    this->matrix.push_back(std::vector<float>(data_points, 0.0f));
+  }
+
+  for (int i = 0; i < data_points; i++) {
+    for (int j = i + 1; j < data_points; j++) {
+      float dx = std::get<0>(data[i]) - std::get<0>(data[j]);
+      float dy = std::get<1>(data[i]) - std::get<1>(data[j]);
+      this->matrix[i][j] = sqrtf(dx * dx + dy * dy);
+      this->matrix[j][i] = this->matrix[i][j];
     }
+  }
 }
 
 float TSP::get_optimal_length(void) {
-    return this->optimal_length;
+  return this->optimal_length;
 }
 
 float TSP::get_distance_between(int a, int b) {
-    return this->matrix[a][b];
+  return this->matrix[a][b];
 }
 
 int TSP::get_number_of_data_points(void) {
-    return this->matrix.size();
+  return this->matrix.size();
 }
 
 int TSP::shortest_distance_from(int src, std::unordered_set<int> &not_included) {
-    std::vector<float> distances = this->matrix[src];
-    int index = 0, best = -1, best_index = -1;
-    for (auto it = distances.begin(); it != distances.end(); ++it) {
-        if (index != src && (best == -1 or *it < best)) {
-            best = *it;
-            best_index = index;
-        }
-        index++;
+  std::vector<float> distances = this->matrix[src];
+  int index = 0, best = -1, best_index = -1;
+  for (auto it = distances.begin(); it != distances.end(); ++it) {
+    if (index != src && (best == -1 or *it < best)) {
+      best = *it;
+      best_index = index;
     }
-    return best_index;
+    index++;
+  }
+  return best_index;
 }
